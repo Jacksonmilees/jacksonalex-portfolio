598f0319ac1e6d4b8a6f6ecf078ae9beeb5d4f8a
Jackson <jacksonmilees@gmail.com>
2025-02-21 11:50:58 +0300

    Updated project files with latest changes

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -2,19 +2,19 @@ export const educations = [
   {
     id: 1,
     title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    duration: "2021 - 2025",
+    institution: "JomoKenyatta University Of Agriculture And Technology",
   },
   {
     id: 2,
     title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    duration: "2017 - 2020",
+    institution: "RIOKINDO SCHOOL",
   },
   {
     id: 3,
-    title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    title: "Primary School Certificate",
+    duration: "2008 - 2016",
+    institution: "Everton Academy Ngong",
   }
 ]
--- utils/data/experience.js
+++ utils/data/experience.js
@@ -2,7 +2,7 @@ export const experiences = [
   {
     id: 1,
     title: 'Software Engineer I',
-    company: "Teton Private Ltd.",
+    company: "4good Private Ltd.",
     duration: "(Jan 2022 - Present)"
   },
   {
@@ -15,6 +15,6 @@ export const experiences = [
     id: 3,
     title: "Self Employed",
     company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
+    duration: "(Jan 2023 - Present)"
   }
 ]
